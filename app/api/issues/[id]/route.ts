c5bcab14c9b9115e739b9bb151f39a22ca2f9612
Guy Brown <guy.brown@justchecking.co.uk>
2024-02-26 20:55:12 +0000

    enhance api to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
@@ -14,9 +14,17 @@ export async function PATCH(
 		}
 
 		const body = await request.json();
-		const validation = issueSchema.safeParse(body)
+		const validation = patchIssueSchema.safeParse(body)
 		if(!validation.success){
 			return NextResponse.json(validation.error.format(), {status: 400});
+		}
+
+		const { assignedToUserId, title, description } = body;
+		if(assignedToUserId){
+			const user = await prisma.user.findUnique({ where: { id: assignedToUserId}});
+			if(!user){
+				return NextResponse.json({ error: 'Invalid user.'}, {status: 400})
+			}
 		}
 
 		const issue = await prisma.issue.findUnique({
@@ -29,8 +37,9 @@ export async function PATCH(
 		const updatedIssue = await prisma.issue.update({
 			where: { id: issue.id },
 			data: {
-				title: body.title,
-				description: body.description,
+				title,
+				description,
+				assignedToUserId
 			}
 		});
 
