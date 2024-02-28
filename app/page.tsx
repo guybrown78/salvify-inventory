a2e5bad8c6adadac2d7f9b800988956a2dc2e648
Guy Brown <guy.brown@justchecking.co.uk>
2024-02-28 12:31:48 +0000

    build the issue chart component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Pagination } from '@/app/components'
 import LatestIssues from './LatestIssues'
 import IssueSummary from './IssueSummary'
 import prisma from '@/prisma/client'
+import IssueChart from './IssueChart'
 
 export default async function Home() {
 
@@ -12,8 +13,9 @@ export default async function Home() {
 
   return (
     <div>
-			<IssueSummary open={open} inProgress={inProgress} closed={closed}/>
+			<IssueSummary open={open} inProgress={inProgress} closed={closed} />
 			<LatestIssues />
+			<IssueChart open={open} inProgress={inProgress} closed={closed} />
 		</div>
   )
 }
