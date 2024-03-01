455bcbdca786f5c9db38e479ef74ae9baf41f3d5
Guy Brown <guy.brown@justchecking.co.uk>
2024-03-01 19:33:25 +0000

    add signed in user name on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,31 @@ import LatestIssues from './LatestIssues'
 import IssueSummary from './IssueSummary'
 import prisma from '@/prisma/client'
 import IssueChart from './IssueChart'
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 import { Flex, Grid } from '@radix-ui/themes'
 import { Metadata } from 'next'
 
 export default async function Home() {
+
+	const session = await getServerSession(authOptions)
 
 	const open = await prisma.issue.count({ where: { status: 'OPEN'}});
 	const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS'}});
 	const closed = await prisma.issue.count({ where: { status: 'CLOSE'}});
 
   return (
-		<Grid columns={{ initial: "1", md: "2"}} gap="5">
-			<Flex direction="column" gap="5">
-				<IssueSummary open={open} inProgress={inProgress} closed={closed} />
-				<IssueChart open={open} inProgress={inProgress} closed={closed} />
-			</Flex>
-			<LatestIssues />
-		</Grid>
+		<>
+			<h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+			<Grid columns={{ initial: "1", md: "2"}} gap="5">
+				<Flex direction="column" gap="5">
+					<IssueSummary open={open} inProgress={inProgress} closed={closed} />
+					<IssueChart open={open} inProgress={inProgress} closed={closed} />
+				</Flex>
+				<LatestIssues />
+			</Grid>
+		</>
+		
   )
 }
 
