14575d33b10ccd3c9124b2a61fc26614a11e11e6
Guy Brown <guy.brown@justchecking.co.uk>
2024-05-23 11:44:22 +0100

    added id to validationToken table

--- prisma/migrations/20240523104353_initial_migration/migration.sql
+++ prisma/migrations/20240523104353_initial_migration/migration.sql
@@ -72,12 +72,14 @@ CREATE TABLE `Client` (
 
 -- CreateTable
 CREATE TABLE `VerificationToken` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(191) NOT NULL,
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
-    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`),
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
