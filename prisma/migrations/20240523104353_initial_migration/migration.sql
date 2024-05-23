db258f2db3ef61654b7aef8ebce441cf32083356
Guy Brown <guy.brown@justchecking.co.uk>
2024-05-23 12:30:01 +0100

    updated migration

--- prisma/migrations/20240523104353_initial_migration/migration.sql
+++ prisma/migrations/20240523104353_initial_migration/migration.sql
@@ -227,9 +227,11 @@ CREATE TABLE `OrderItem` (
 
 -- CreateTable
 CREATE TABLE `_OptionalClients` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `A` INTEGER NOT NULL,
     `B` VARCHAR(191) NOT NULL,
-
+    
+    PRIMARY KEY (`id`),
     UNIQUE INDEX `_OptionalClients_AB_unique`(`A`, `B`),
     INDEX `_OptionalClients_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
