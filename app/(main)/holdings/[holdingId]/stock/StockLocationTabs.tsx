ec678b4ab22611b2011359c5ec5070521fd24e7c
Guy Brown <guy.brown@justchecking.co.uk>
2024-06-17 16:26:24 +0100

    added holding > location tabs horizontal scroll

--- app/(main)/holdings/[holdingId]/stock/StockLocationTabs.tsx
+++ app/(main)/holdings/[holdingId]/stock/StockLocationTabs.tsx
@@ -1,52 +1,56 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Button, Flex, Tabs, Box, Text } from '@radix-ui/themes'
-import Link from 'next/link'
-import { Location } from '@prisma/client'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useHoldingContext } from '@/app/_providers/HoldingProvider'
-
-interface Props{
-	locations:Location[]
+import { useHoldingContext } from "@/app/_providers/HoldingProvider";
+import { Location } from "@prisma/client";
+import * as ScrollArea from "@radix-ui/react-scroll-area";
+import { Flex, Tabs } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
+import './styles.css';
+interface Props {
+	locations: Location[];
 }
 
 const StockLocationTabs = ({ locations }: Props) => {
-
 	const router = useRouter();
 	const searchParams = useSearchParams();
-	const { currentHolding } = useHoldingContext()
+	const { currentHolding } = useHoldingContext();
 	return (
-		<Flex justify='between'>
-			<Tabs.Root 
-				className='w-full'
-				defaultValue={searchParams.get('location') || ''}
-				onValueChange={(location) => {
-					const params = new URLSearchParams()
-					if(location) 
-						params.append('location', location);
-					// add order / pag etc
-					const query = params.size ? '?' + params.toString() : '';
-					router.push(`/holdings/${currentHolding!.id}/stock` + query)
-				}}
+		<ScrollArea.Root className="ScrollAreaRoot">
+			<ScrollArea.Viewport className="ScrollAreaViewport">
+				{/*  */}
+				<Flex justify="between">
+					<Tabs.Root
+						className="w-full"
+						defaultValue={searchParams.get("location") || ""}
+						onValueChange={(location) => {
+							const params = new URLSearchParams();
+							if (location) params.append("location", location);
+							// add order / pag etc
+							const query = params.size ? "?" + params.toString() : "";
+							router.push(`/holdings/${currentHolding!.id}/stock` + query);
+						}}
+					>
+						<Tabs.List>
+							<Tabs.Trigger value="">All</Tabs.Trigger>
+							{locations.map((location) => (
+								<Tabs.Trigger key={location.id} value={String(location.id)}>
+									{location.title}
+								</Tabs.Trigger>
+							))}
+						</Tabs.List>
+					</Tabs.Root>
+				</Flex>
+				{/*  */}
+			</ScrollArea.Viewport>
+			<ScrollArea.Scrollbar
+				className="ScrollAreaScrollbar"
+				orientation="horizontal"
 			>
-				<Tabs.List>
-					<Tabs.Trigger value="">All</Tabs.Trigger>
-					{
-						locations.map(location => 
-							<Tabs.Trigger 
-								key={location.id} 
-								value={String(location.id)}
-							>
-								{location.title}
-							</Tabs.Trigger>)
-					}
-				</Tabs.List>
-			</Tabs.Root>
+				<ScrollArea.Thumb className="ScrollAreaThumb" />
+			</ScrollArea.Scrollbar>
+			<ScrollArea.Corner className="ScrollAreaCorner" />
+		</ScrollArea.Root>
+	);
+};
 
-
-		</Flex>
-	)
-}
-
-export default StockLocationTabs
+export default StockLocationTabs;
